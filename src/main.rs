063089dda6503db243a6503d7e8f4e5448c10e88
f0reachARR <f0reach@f0reach.me>
2022-08-06 13:11:36 +0900

    無限ループを直した

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,7 @@ impl<'a> Tokenizer<'a> {
                 if *c != *start {
                     str.push(*c);
                 } else {
+                    self.cursor = cursor;
                     return Some(PositionedToken(Token::String(str), pos));
                 }
             }
